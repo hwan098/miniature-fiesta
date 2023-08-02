29415e47df67d5cf59cf695d41c339cdcd90e84f
Choi Changhwan <glqkfl7845@gmail.com>
2023-08-02 20:33:55 +0900

    Project 생성
    hello world

---
+++ src/main/java/net/javaguides/springboot/controller/HelloWorldController.java
@@ -0,0 +1,9 @@
+package net.javaguides.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller //이 컨트롤러 클래스를 MVC컨트롤러로 만들겠다.
+@ResponseBody //JAVA 객테에서 JSON으로 응답을 변환.
+public class HelloWorldController {
+}
