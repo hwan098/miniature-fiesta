5c408466b82088fdd1ce6e9a1d615fb167512994
Choi Changhwan <glqkfl7845@gmail.com>
2023-08-03 19:45:09 +0900

    Delete Request
    @DeleteMapping 어노테이션을 이용하여 HTTP DELETE 요청을 처리하는 API를 생성, postman으로 확인

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -70,4 +70,11 @@ public class StudentController {
         System.out.println(student.getLastName());
         return student;
     }
+
+    //Spring Boot REST API that handles HTTP DELETE Request - deleting the existing resource
+    @DeleteMapping("students/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId){
+        System.out.println(studentId);
+        return "Student deleted successfully!";
+    }
 }
