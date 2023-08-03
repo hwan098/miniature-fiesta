1b3096fa062855da620ed79d664ae5c486bd437f
Choi Changhwan <glqkfl7845@gmail.com>
2023-08-03 12:20:17 +0900

    returns List
     List로 클라이언트에 잘 반환이 되는지 브라우저에서 확인

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -4,6 +4,10 @@ import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -15,5 +19,14 @@ public class StudentController {
             "Choi"
         );
         return student;
+    }
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Changhwan", "Choi"));
+        students.add(new Student(2, "Changhwan", "kim"));
+        students.add(new Student(3, "Changhwan", "lee"));
+        students.add(new Student(4, "Changhwan", "park"));
+        return students;
     }
 }
